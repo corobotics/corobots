be15bcc233e5899e6502d8c303c73e78428f5d14
Max Bogue <max@maxbogue.com>
2012-11-28 12:53:34 -0500

    Add some comments to the obstacle avoidance header.

--- obstacle_avoidance/include/obstacle_avoidance.h
+++ obstacle_avoidance/include/obstacle_avoidance.h
@@ -8,14 +8,42 @@
 #include "geometry_msgs/Pose2D.h"
 #include "sensor_msgs/LaserScan.h"
 
+/**
+ * Abstract class to handle avoiding obstacles.
+ */
 class ObstacleAvoider {
 public:
+    /**
+     * Abstract method to provide a navigation vector based off a laser scan.
+     *
+     * @param scan  The laser scan to navigate with.
+     * @returns     A 2D movement vector.
+     */
     virtual geometry_msgs::Point nav(sensor_msgs::LaserScan scan) = 0;
+
+    /**
+     * Update the known pose of the robot.
+     *
+     * @param pose  The new pose of the robot.
+     */
     void updatePose(geometry_msgs::Pose2D pose);
+
+    /**
+     * Add a waypoint to the queue.
+     *
+     * @param waypoint  The new waypoint for the queue.
+     */
     void addWaypoint(geometry_msgs::Point waypoint);
 
 protected:
+    /**
+     * The current pose of the robot.
+     */
     geometry_msgs::Pose2D pose;
+
+    /**
+     * A queue of waypoints for the robot to navigate to.
+     */
     std::queue<geometry_msgs::Point> waypointQueue;
 };
 
