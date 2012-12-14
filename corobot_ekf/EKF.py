c108b7785833f458d27e8b02404f40ab4a61a4ca
Max Bogue <max@maxbogue.com>
2012-12-14 17:19:32 -0500

    Untested prediction code.

--- corobot_ekf/EKF.py
+++ corobot_ekf/EKF.py
@@ -1,36 +1,59 @@
+from math import cos, sin
 
 class EKF(object):
 
     """Implements an Extended Kalman Filter.
 
-    Follows the EKF described in Priciples of Robotic Motion.
+    Follows the EKF described in Principles of Robotic Motion.
 
     """
 
-    def __init__(self):
-        self.initialized = False
+    def __init__(self, dt):
 
-        # x(k+1|k+1); the system state.
-        self.x = np.zeros(3)
+        # Time delta between updates.
+        self.dt = dt
 
-        # P(k+1|k+1); the system covariance matrix.
-        self.P = matrix([[10000.0, 0.0, 0.0],
-                         [0.0, 10000.0, 0.0],
-                         [0.0, 0.0, 10000.0]])
+        # x(k|k); the system state.
+        x = np.zeros(3)
+
+        # P(k|k); the system covariance matrix.
+        P = array([
+            [10000.0, 0.0, 0.0],
+            [0.0, 10000.0, 0.0],
+            [0.0, 0.0, 10000.0]])
+
+        self.state = (x, P)
 
         # x(k+1|k); the state prediction.
-        self.xp = np.zeros(3)
+        xp = np.zeros(3)
 
         # P(k+1|k); the covariance prediction.
-        self.PP = matrix([[10000.0, 0.0, 0.0],
-                          [0.0, 10000.0, 0.0],
-                          [0.0, 0.0, 10000.0]])
+        PP = array([
+            [10000.0, 0.0, 0.0],
+            [0.0, 10000.0, 0.0],
+            [0.0, 0.0, 10000.0]])
 
-    def update(pose):
-        pass
+        self.pred = (xp, PP)
 
-    def predict():
-        pass
+        # Tracks the previous state of sensors.
+        self.old = {}
+
+    # update :: string -> Pose -> void
+    def update(self, sensor, pose):
+        if sensor not in self.old:
+            return
+
+
+    def predict(self, stamp, u, V):
+        (x, y, t), P = self.state
+        v, w = u
+        xp = array([x + v * cos(t), y + self.dt * v * sin(t), t + self.dt * w])
+        F = array([
+            [1, 0, -self.dt * v * sin(t)],
+            [0, 1, selt.dt * v * cos(t)],
+            [0, 0, 1]])
+        PP = F * P * np.transpose(F) + V
+        return xp, PP
 
     def R():
         pass
