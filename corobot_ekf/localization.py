11d7a1adbcb915bb6adadc2465bc03e77641ac59
Max Bogue <max@maxbogue.com>
2013-01-17 12:31:57 -0500

    Add a conversion function to pull a Corobot::Pose out of an Odom message.

--- corobot_ekf/localization.py
+++ corobot_ekf/localization.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 
+from math import atan2
+
 import roslib; roslib.load_manifest("corobot_ekf")
 import rospy
 
 from corobot_msgs.msg import Pose
 from nav_msgs.msg import Odometry
 
+def convert_to_pose(odom):
+    pose = Pose()
+    pose.header = odom.header
+    pose.x = odom.pose.position.x
+    pose.y = odom.pose.position.y
+    qz = odom.pose.orientation.z
+    qw = odom.pose.orientation.w
+    pose.theta = atan2(0, w * w - z * z)
+    cov = odom.pose.covariance
+    pose.cov = (
+        cov[0],  cov[1],  cov[5],
+        cov[6],  cov[7],  cov[11],
+        cov[30], cov[31], cov[35])
+    return pose
+
 def odom_callback(odom):
-    pub.publish(odom.pose)
+    pub.publish(convert_to_pose(odom))
 
 def main():
     global pub;
