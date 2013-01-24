8c476ec68d8f1f5bead03fba9e6546933186d952
Karl Berger <krab.berger@gmail.com>
2013-01-24 11:40:20 -0500

    Waypoint parsing

--- corobot_map/src/map.py
+++ corobot_map/src/map.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('corobot_map')
 import rospy
+import sys
 
 from nav_msgs.srv import *
 from corobot_msgs.msg import Waypoint
 from corobot_msgs.srv import *
+
+wploc = roslib.packages.get_pkg_dir('corobot_map') + "/map/locations.csv"
+wps = {}
 
 def handle_get_map(req):
 	return map_server_client()
@@ -19,16 +23,31 @@ def map_server_client():
 		static_map = rospy.ServiceProxy('static_map', GetMap)
 		resp = static_map()
 		return GetMapResponse(resp.map)
-	except: rospy.ServiceException, e:
+	except rospy.ServiceException, e:
 		print "Service call failed: %s"%e
 
 def get_waypoints():
-	return 
+	return None
+
+def get_neighbors(req):
+	return None
+
+def loadWaypoints():
+	with open(wploc, 'r') as wpFile:
+		for line in wpFile:
+			vals = line.split(',')
+			neighborList = []
+			for neighbor in vals[6:]:
+				if neighbor != "":
+					neighborList.append(neighbor)
+			wps[vals[0]]=vals[3],vals[4],neighborList
 
 '''This node is acting as a relay for image-driven map data and then
 ties in our waypoint/location data'''
 def main():
-	#TODO: Add locations.csv parsing and graph generation.
+	#locations.csv parsing.
+	loadWaypoints()
+
 	rospy.init_node('corobot_map_server')
 	rospy.Service('get_map', GetMap, handle_get_map)
 	rospy.Service('get_waypoints', GetWaypoints, handle_get_waypoints)
