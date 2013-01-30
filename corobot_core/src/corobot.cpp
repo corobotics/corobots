79c11e2c348b631de85f5ddef7b88ea9960e532d
Max Bogue <max@maxbogue.com>
2013-01-30 15:59:22 -0500

    Add a few more helper functions to corobot_core.

--- corobot_core/src/corobot.cpp
+++ corobot_core/src/corobot.cpp
@@ -1,9 +1,34 @@
 #include <cmath>
 
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Quaternion.h"
+
 #include "corobot.h"
 
 namespace corobot {
+
     double dist(double x, double y) {
         return sqrt(x * x + y * y);
     }
+
+    double quaternionToRads(geometry_msgs::Quaternion q) {
+        return atan2(2 * q.w * q.z, 1 - 2 * q.z * q.z);
+    }
+
+    SimplePose geomPoseToSimplePose(geometry_msgs::Pose pose) {
+        SimplePose simplePose;
+        simplePose.x = pose.position.x;
+        simplePose.y = pose.position.y;
+        simplePose.a = quaternionToRads(pose.orientation);
+        return simplePose;
+    }
+
+    SimplePose coordTransform(SimplePose state, SimplePose offset) {
+        SimplePose result;
+        result.x = state.x * cos(offset.a) - state.y * sin(offset.a) + offset.x;
+        result.y = state.x * sin(offset.a) + state.y * cos(offset.a) + offset.y;
+        result.a = state.a + offset.a;
+        return result;
+    }
+
 }
