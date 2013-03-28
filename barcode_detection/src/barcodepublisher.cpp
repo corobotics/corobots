ae583ac9d09aa74c5e9f274166eb7b73fe714681
ravdeepjohar <ravdeepsinghjohar@live.com>
2013-03-28 13:13:10 -0400

    fix offset

--- barcode_detection/src/barcodepublisher.cpp
+++ barcode_detection/src/barcodepublisher.cpp
@@ -20,23 +20,24 @@ using corobot_msgs::Pose;
 int main(int argc, char **argv)
 {
     
-       
+     
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise <std_msgs::String > ("chatter", 1000);
+    ros::Publisher chatter_pub = n.advertise <Pose> ("chatter", 1000);
 
     // std_msgs::String 
     // create and initialize a Processor
     const char *device = "/dev/video0";
 
-
+   
     Processor proc;
+    //Don't change the resolution, will screw up everything!
     proc.request_size(1600,1200);
     proc.init(device,true);
 
     // configure the Processor
-    proc.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
-
+    proc.set_config(ZBAR_QRCODE, ZBAR_CFG_ENABLE, 1);
+	 
     // setup a callback
     BarcodeHandler my_handler(chatter_pub);
     proc.set_handler(my_handler);
@@ -44,13 +45,16 @@ int main(int argc, char **argv)
     // enable the preview window
     proc.set_visible();
     proc.set_active();
-
+	
     try {
 	// keep scanning until user provides key/mouse input
-	proc.user_wait();
+	
+	proc.process_one();
     }
     catch(ClosedError & e) {
     }
+	
+	
     return (0);
 }
 
