1b603b8ce2a31e81eeeeae25612f9a9d5e5ab543
Karl Berger <krab.berger@gmail.com>
2013-04-04 11:17:54 -0400

    Adding client communication test script in case anyone finds it useful.

---
+++ corobot_comm/test/test_client.py
@@ -0,0 +1,30 @@
+import socket
+
+def main():
+    host = socket.gethostbyname(socket.gethostname())
+    print(host)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((host,15001))
+    print("CONNECTED")
+    
+    s.sendall('GETPOS\n')
+    data = s.recv(1024)
+    print(data)
+    
+    s.sendall('GOTOLOC VENDDOOR\n')
+    data = s.recv(1024)
+    print(data)
+
+    s.sendall('NAVTOLOC VENDDOOR\n')
+    data = s.recv(1024)
+    print(data)
+
+    #Should return an ERROR condition
+    s.sendall('NAVTOLOC NOWHERE\n')
+    data = s.recv(1024)
+    print(data)
+
+    s.close()
+
+if __name__ == '__main__':
+    main()
