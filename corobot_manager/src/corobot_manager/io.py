26e39f674c7fa043517dca5c3654ece1cc9c9042
Max Bogue <max@maxbogue.com>
2013-04-15 14:38:43 -0400

    Overhaul corobot_manager to use asyncore/asynchat.

---
+++ corobot_manager/src/corobot_manager/io.py
@@ -0,0 +1,65 @@
+import socket
+import traceback
+from asynchat import async_chat, simple_producer
+from asyncore import dispatcher
+
+class CorobotServer(dispatcher):
+
+    def __init__(self, port, manager):
+        dispatcher.__init__(self)
+        self.manager = manager
+        self.handler = None
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Allows a socket still in the TIME_WAIT state to be used.
+        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bind((socket.gethostname(), port))
+        self.listen(1)
+
+    def handle_error(self):
+        traceback.print_exc()
+        self.manager.shutdown()
+
+    def handle_accept(self):
+        sock, addr = self.accept()
+        if self.handler is None:
+            self.handler = CorobotHandler(self, sock)
+        else:
+            # Can only have one client connected at a time.
+            sock.sendall("INUSE\n")
+            sock.close()
+
+class LineHandler(async_chat):
+
+    """Reads lines and calls a callback with them."""
+
+    def __init__(self, sock, line_read):
+        async_chat.__init__(self, sock=sock)
+        self.line_read = line_read
+        self.received_data = []
+        self.set_terminator("\n")
+
+    def write_line(self, msg):
+        self.producer_fifo.append(simple_producer(msg + "\n"))
+
+    def collect_incoming_data(self, data):
+        self.received_data.append(data)
+
+    def found_terminator(self):
+        line = "".join(self.received_data)
+        self.line_read(line)
+
+    def handle_error(self):
+        traceback.print_exc()
+        self.handle_close()
+
+class CorobotHandler(LineHandler):
+
+    """Customize LineHandler for Corobot specific things."""
+
+    def __init__(self, server, sock):
+        LineHandler.__init__(self, sock, server.manager.handle_command)
+        self.server = server
+
+    def handle_close(self):
+        self.server.handler = None
+        self.close()
--- corobot_manager/src/manage.py
+++ corobot_manager/src/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import asyncore
 from collections import deque
 from contextlib import closing
 import socket
@@ -13,6 +14,7 @@ from geometry_msgs.msg import Point
 from corobot_common import point_equals
 from corobot_common.srv import GetLandmark
 from corobot_common.msg import Pose, Landmark
+from corobot_manager.io import CorobotServer
 
 class CorobotManager():
 
@@ -21,10 +23,8 @@ class CorobotManager():
         self.pose = Pose(x=67.7648,y=14.9568,theta=0) # Close to EInter
         # Track goals.
         self.goal_queue = deque()
-        # The output stream to the current client, or None.
-        self.client_out = None
-        # A lock so we can write to the client from multiple threads.
-        self.client_out_lock = threading.Lock()
+        # The asyncore.dispatcher object wrapping our server socket.
+        self.server = None
 
     def start(self):
         self.init_ros_node()
@@ -33,12 +33,18 @@ class CorobotManager():
         except SystemExit:
             rospy.signal_shutdown("Requested by user.")
 
+    def shutdown(self):
+        if not rospy.is_shutdown():
+            rospy.signal_shutdown("")
+        if self.server is not None:
+            self.server.handle_close()
+            if self.server.handler is not None:
+                self.server.handler.close_when_done()
+
     def client_write(self, msg_id, msg):
         """Utility function to write a message to the current client."""
-        with self.client_out_lock:
-            if self.client_out:
-                self.client_out.write("%s %s\n" % (msg_id, msg))
-                self.client_out.flush()
+        if self.server.handler:
+            self.server.handler.write_line("%s %s" % (msg_id, msg))
 
     def pose_callback(self, pose):
         """Callback for the pose ROS topic."""
@@ -60,82 +66,44 @@ class CorobotManager():
         self.goals_pub = rospy.Publisher("goals", Point)
         self.goals_nav_pub = rospy.Publisher("goals_nav", Point)
         rospy.loginfo("Listening for client robots.")
+        rospy.on_shutdown(self.shutdown)
 
     def listen_for_clients(self):
-        # Create our server socket.
-        server_socket = socket.socket()
-        server_socket.bind((socket.gethostname(), 15001))
-        server_socket.listen(1)
-        while not rospy.is_shutdown():
-            # Accept socket.
-            try:
-                client_socket, client_addr = server_socket.accept()
-            except socket.error as e:
-                # If a SystemExit is raised during socket.accept(), it gets
-                # rethrown as a socket.error, but we want SystemExit.
-                raise SystemExit(e)
-            with closing(client_socket):
-                # Set up the output output stream variable.
-                with self.client_out_lock:
-                    self.client_out = client_socket.makefile("w")
-                # Only this function gets the input stream.
-                with closing(client_socket.makefile("r")) as client_in:
-                    try:
-                        self.listen_for_commands(client_in, client_addr)
-                    except socket.error as e:
-                        rospy.logerr(e)
-                # Clean up client_out.
-                with self.client_out_lock:
-                    try:
-                        self.client_out.close()
-                    except:
-                        pass
-                    self.client_out = None
+        self.server = CorobotServer(15001, self)
+        asyncore.loop(0.1)
 
-    def listen_for_commands(self, client_in, client_addr):
-        """Listen for API commands from the client.
+    def handle_command(self, command):
+        rospy.loginfo("Command recieved from client: %s", command)
+        tokens = command.strip().split(" ")
+        if not tokens:
+            self.server.handler.close_when_done()
+            return
+        msg_id = tokens[0]
+        if len(tokens) < 2:
+            self.client_write(msg_id, "ERROR Missing message type.")
+            return
+        msg_type = tokens[1]
+        data = tokens[2:]
 
-        client_in -- A file object for reading from the client socket.
-        client_addr -- The client's address.
-
-        """
-        rospy.loginfo("Ready for commands.")
-        while not rospy.is_shutdown():
-            command = client_in.readline()
-
-            # Communication terminated?
-            if len(command) == 0:
-                break
-
-            host, port = client_addr
-            rospy.loginfo("Command recieved from client [%s:%s]: %s", host, port, command)
-            tokens = command.strip().split(" ")
-            msg_id = tokens[0]
-            if len(tokens) < 2:
-                self.client_write(msg_id, "ERROR Missing message type.")
-                continue
-            msg_type = tokens[1]
-            data = tokens[2:]
-
-            #Command processing
-            if msg_type == "GETPOS":
-                self.client_write(msg_id, "POS %f %f %f" % (self.pose.x, self.pose.y, self.pose.theta))
-            elif msg_type.startswith(("GOTO", "NAVTO")):
-                if msg_type.endswith("LOC"):
-                    try:
-                        landmark = self.get_landmark(data[0].upper())
-                        x, y = landmark.wp.x, landmark.wp.y
-                    except rospy.ServiceException as e:
-                        self.client_write(msg_id, "ERROR " % e)
-                else:
-                    x, y = float(data[0]), float(data[1])
-                if msg_type.startswith("NAVTO"):
-                    self.goals_nav_pub.publish(x=x, y=y)
-                else:
-                    self.goals_pub.publish(x=x, y=y)
-                self.goal_queue.append((msg_id, Point(x=x, y=y)))
+        #Command processing
+        if msg_type == "GETPOS":
+            self.client_write(msg_id, "POS %f %f %f" % (self.pose.x, self.pose.y, self.pose.theta))
+        elif msg_type.startswith(("GOTO", "NAVTO")):
+            if msg_type.endswith("LOC"):
+                try:
+                    landmark = self.get_landmark(data[0].upper())
+                    x, y = landmark.wp.x, landmark.wp.y
+                except rospy.ServiceException as e:
+                    self.client_write(msg_id, "ERROR " % e)
             else:
-                self.client_write(msg_id, "ERROR Unknown message type \"%s\"" % msg_type)
+                x, y = float(data[0]), float(data[1])
+            if msg_type.startswith("NAVTO"):
+                self.goals_nav_pub.publish(x=x, y=y)
+            else:
+                self.goals_pub.publish(x=x, y=y)
+            self.goal_queue.append((msg_id, Point(x=x, y=y)))
+        else:
+            self.client_write(msg_id, "ERROR Unknown message type \"%s\"" % msg_type)
 
 def main():
     CorobotManager().start()
