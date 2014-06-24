b018542445b5cb88e8f844ef8a2a0aa8d8242eca
corobot <corobot@corobot3.(none)>
2014-06-24 16:48:02 -0400

    Adding more options and data collection to spin

--- corobot_spin/src/spin.py
+++ corobot_spin/src/spin.py
@@ -5,7 +5,7 @@ import sys
 from corobot_common.msg import Pose
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from math import atan2,pi
+from math import atan2,pi,sqrt
 
 pub = rospy.Publisher("cmd_vel", Twist)
 
@@ -21,42 +21,51 @@ def odom_to_pose(odom):
     
 class spin():
     def __init__(self, nTurns, timePerTurn, angularV):
-	self.nTurns = nTurns
-	self.timePerTurn = timePerTurn
-	self.angularV = angularV
+        self.nTurns = nTurns
+        self.timePerTurn = timePerTurn
+        self.angularV = angularV
+        self.pose = Pose()
+        self.traveled = 0
+        self.observed = 0
 
     def pose_callback(self, pose):
-	self.observed =odom_to_pose(pose).theta
+        self.lastpose = self.pose
+        self.pose = odom_to_pose(pose)
+        self.observed = self.pose.theta
+        self.traveled += sqrt(pow(self.lastpose.x-self.pose.x,2)+pow(self.lastpose.y-self.pose.y,2))
 
     def pause_callback(self, event):
-	rospy.Timer(rospy.Duration(secs = self.timePerTurn),self.timer_callback,True)
-	self.done = False
-	go = Twist()
-	go.linear.x = 0
-	go.angular.z = self.angularV
-	while(not self.done):
-  	    pub.publish(go)
+        rospy.Timer(rospy.Duration(secs = self.timePerTurn),self.timer_callback,True)
+        self.done = False
+        go = Twist()
+        go.linear.x = 0 # fwd velocity during turning - make a parameter?
+        go.angular.z = self.angularV
+        rospy.loginfo("Theta: %g, Dist traveled: %g",self.observed,self.traveled)
+        while(not self.done):
+              pub.publish(go)
 
     def timer_callback(self, event):
-	if(self.nTurns  > 1):
-	    rospy.Timer(rospy.Duration(secs = .5),self.pause_callback,True)
-	self.nTurns-=1
-	self.done = True
-	go = Twist()
-	go.linear.x = 0
-	go.angular.z = 0
-	rospy.loginfo(self.observed)
-	while(self.done):
-	    pub.publish(go)
-	
+        self.nTurns-=1
+        self.done = True
+        go = Twist()
+        go.linear.x = 0 # fwd velocity between turns - make a parameter?
+        go.angular.z = 0
+        if(self.nTurns  >= 1):
+            rospy.Timer(rospy.Duration(secs = .5),self.pause_callback,True)
+        else:
+            go.linear.x = 0
+        rospy.loginfo("Theta: %g, Dist traveled: %g",self.observed,self.traveled)
+        while(self.done):
+            pub.publish(go)
+        
 
     def start(self):
-	self.done = False
-	rospy.init_node("rotate")
-	rospy.Subscriber("odom", Odometry, self.pose_callback)
-	self.expected = self.nTurns * self.timePerTurn * self.angularV
-	self.pause_callback(None)
-	rospy.spin()
+        self.done = False
+        rospy.init_node("rotate")
+        rospy.Subscriber("odom", Odometry, self.pose_callback)
+        self.expected = self.nTurns * self.timePerTurn * self.angularV
+        self.pause_callback(None)
+        rospy.spin()
 
 
 def main():
